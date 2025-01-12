947cabe38b77b7b25e3baa183d4e608f025632a9
HananDeeplify <hanan.javaid@deeplify.de>
2025-01-12 21:24:09 +0100

    Add initial dataset and analysis for screen time impact on sleep quality

---
+++ R Code/analysis.r
@@ -0,0 +1,45 @@
+# Load necessary libraries
+library(stats)
+library(pROC)
+
+# Please here provide path to your data file
+sleep_data <- read.csv("C:/Users/metro/iCloudDrive/FUAS/IDA/Report/R Code/sleep_data.csv") 
+
+# Here I set the Sleep_Quality to a binary
+sleep_data$Sleep_Quality <- ifelse(sleep_data$Sleep_Quality == "Good", 1, 0)
+
+model <- glm(Sleep_Quality ~ Screen_Time, data = sleep_data, family = binomial)
+
+model_summary <- summary(model)
+print(model_summary)
+
+# odds ratios
+odds_ratios <- exp(coef(model))
+print(odds_ratios)
+
+conf_intervals <- exp(confint(model))
+print(conf_intervals)
+
+# Calculate AIC
+aic_value <- AIC(model)
+print(paste("AIC:", aic_value))
+
+# ROC curve
+roc_curve <- roc(sleep_data$Sleep_Quality, fitted(model))
+png("chart.png")
+plot(1 - roc_curve$specificities, roc_curve$sensitivities, type = "l", main = "ROC Curve for Logistic Regression Model",
+     xlab = "1 - Specificity", ylab = "Sensitivity", xlim = c(0, 1), ylim = c(0, 1))
+dev.off()
+auc_value <- auc(roc_curve)
+print(paste("AUC:", auc_value))
+
+sensitivity <- roc_curve$sensitivities[which.max(roc_curve$sensitivities + roc_curve$specificities - 1)]
+specificity <- roc_curve$specificities[which.max(roc_curve$sensitivities + roc_curve$specificities - 1)]
+print(paste("Sensitivity:", sensitivity))
+print(paste("Specificity:", specificity))
+
+png("logistic_regression_plot.png")
+plot(sleep_data$Screen_Time, sleep_data$Sleep_Quality, main = "Logistic Regression of Sleep Quality on Screen Time",
+     xlab = "Screen Time (hours)", ylab = "Probability of Good Sleep Quality", xlim = c(0, max(sleep_data$Screen_Time)), ylim = c(0, 1))
+curve(predict(model, data.frame(Screen_Time = x), type = "response"), add = TRUE)
+dev.off()
---
+++ README.md
@@ -0,0 +1,28 @@
+# Effect of Screen Time on Quality of Sleep
+
+This project investigates the effect of screen time before bed on sleep quality using logistic regression analysis.
+
+## Files
+
+- `R Code/analysis.r`: R script for data analysis and visualization.
+- `report.tex`: LaTeX source file for the report.
+- `report.pdf`: Compiled PDF of the report.
+- `.gitignore`: Specifies files and directories to be ignored by Git.
+
+## Usage
+
+### Data Analysis
+
+1. Ensure you have R and the necessary libraries (`ggplot2`, `dplyr`, `caret`) installed.
+2. Run the R script `R Code/analysis.r` to perform the data analysis and generate the model performance metrics.
+
+### Report Compilation
+
+1. Ensure you have a LaTeX distribution (e.g., MiKTeX, TeX Live) installed.
+2. Compile the `report.tex` file using a LaTeX editor or command line to generate the `report.pdf`.
+
+## Authors
+
+- Abdul Hanan Javaid
+- Muhammad Saad Zafar
+- Tehreem Iftikhar
